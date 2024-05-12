4d5c6fe0c91ac745e407d21018549f60b6efda60
David Garizado <dgarizad@c1r15s2.42malaga.com>
2024-05-12 13:38:58 +0200

    updated

--- includes/ConfParse.hpp
+++ includes/ConfParse.hpp
@@ -1,27 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   webserver.hpp                                      :+:      :+:    :+:   */
+/*   ConfParse.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dgarizad <dgarizad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/11 21:31:14 by dgarizad          #+#    #+#             */
-/*   Updated: 2024/05/11 21:40:58 by dgarizad         ###   ########.fr       */
+/*   Created: 2024/05/12 12:12:00 by dgarizad          #+#    #+#             */
+/*   Updated: 2024/05/12 12:44:26 by dgarizad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//HERE WE WILL INCLUDE ALL THE HEADER FILES NEEDED FOR THE WEB SERVER
-#ifndef WEBSERVER_HPP
-# define WEBSERVER_HPP
+#ifndef CONFPARSE_HPP
+# define CONFPARSE_HPP
+# include "webserv.hpp"
 
-# include <iostream>
-# include <string>
-# include <vector>
-# include <map>
-# include <fstream>
-# include <sstream>
-# include <sys/socket.h>
-# include <netinet/in.h>
-# include <arpa/inet.h>
-# include <unistd.h>
+class ConfParse
+{
+	public:
+		ConfParse();
+		ConfParse(std::string const &config_file);
+		~ConfParse();
+		ConfParse(ConfParse const &src);
+		ConfParse &operator=(ConfParse const &src);
+
+		bool readConfigFile() const;
+	private:
+		std::string _config_file;
+};
+
 #endif
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,33 +6,35 @@
 /*   By: dgarizad <dgarizad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/11 21:41:19 by dgarizad          #+#    #+#             */
-/*   Updated: 2024/05/11 21:50:46 by dgarizad         ###   ########.fr       */
+/*   Updated: 2024/05/12 13:06:21 by dgarizad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/webserver.hpp"
+#include "../includes/webserv.hpp"
+#include "../includes/ConfParse.hpp"
 
 int main(int argc, char **argv)
 {
 	if (argc > 2)
 	{
-		std::cerr << "Usage: ./webserver [config_file]" << std::endl;
+		std::cerr << RED << "Usage: ./webserver [config_file]" << RESET <<std::endl;
 		return (1);
 	}
-	//first we will read the config file and store it in a string parsed.
 	if (argc == 2)
 	{
-		std::string config_file = argv[1];
-		std::ifstream file(config_file.c_str());
-		if (!file.is_open())
+		
+		std::string config_file(argv[1]);
+		ConfParse conf(config_file);
+		if (!conf.readConfigFile())
 		{
-			std::cerr << "Error: could not open file " << config_file << std::endl;
 			return (1);
 		}
-		std::stringstream buffer;
-		buffer << file.rdbuf();
-		std::string parsed = buffer.str();
-		file.close();
+		return (0);
+	} 
+	else
+	{
+		std::cout << GREEN << "Usign Default config file: ./config/default.conf"<< RESET<< std::endl;
+		//USE DEFAULT CONFIG FILE
 	}
 	return (0);
 }
